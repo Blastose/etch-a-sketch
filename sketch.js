be7cb5d5c011f118b918160bbb2541b7e288d9d1
Blastose <blastoisejay@gmail.com>
2022-05-02 10:14:09 -0700

    Add clear button to clear canvas

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-function createGrid(dimension, gridSize) {
+function createCanvas(dimension, gridSize) {
   const playArea = document.getElementById('play-area');
   const numTiles = dimension * dimension;
   for (let i = 0; i < numTiles; i++) {
@@ -7,17 +7,17 @@ function createGrid(dimension, gridSize) {
     console.log(gridSize / dimension);
     tile.style.height = `${gridSize / dimension}px`;
     tile.style.width = `${gridSize / dimension}px`;
-    tile.addEventListener('mousedown', (e) => {
+    tile.addEventListener('mouseover', (e) => {
       e.target.style['background-color'] = tileColor;
     });
     playArea.appendChild(tile);
   }
 }
 
-function clearGrid() {
+function clearCanvas() {
   const tiles = document.querySelectorAll('.grid-tile');
   tiles.forEach((tile) => {
-    tile.remove();
+    tile.style['background-color'] = "";
   })
 }
 
@@ -25,9 +25,11 @@ function updateColorFromColorPicker(e) {
   tileColor = e.target.value;
 }
 
-const gridDimension = 8;
+const gridDimension = 24;
 const gridSize = 499;
-createGrid(gridDimension, gridSize);
+createCanvas(gridDimension, gridSize);
 const colorPicker = document.getElementById('color-pick-main');
 colorPicker.addEventListener('change', updateColorFromColorPicker);
 let tileColor = colorPicker.value;
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearCanvas);
